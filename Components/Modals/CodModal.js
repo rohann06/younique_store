2399c17a5fec700484410e8ef18b8a2b1f51c2d0
Rohan <rohanahire651@gmail.com>
2023-01-13 22:00:59 +0530

    link and size form completed

--- Components/Modals/CodModal.js
+++ Components/Modals/CodModal.js
@@ -1,24 +1,34 @@
-const CodModal = ({isVisble, onClose}) => {
+const CodModal = ({ isVisble, onClose }) => {
+  if (!isVisble) return null;
 
-    if(!isVisble) return null;
+  return (
+    <div className=" fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
+      <div className=" w-full mx-8 lg:w-[600px]">
+        <div className=" bg-slate-100 p-2 rounded-lg">
+          <div className=" flex justify-between px-2 border-b-2 pb-2 text-black">
+            <p className=" font-Poppins font-medium ">Payment method</p>
 
-    return ( 
-         <div className=" fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
-            <div className=" w-full mx-8 lg:w-[600px]">
-                <div className=" bg-slate-100 p-2 rounded-lg">
-                    <div className=" flex justify-between px-2 border-b-2 pb-2 text-black">
-
-                        <p className=" font-Poppins font-medium ">Payment method</p>
-
-                        <button onClick={()=> onClose()} className=" font-bold text-sm border-2 px-2 rounded-md hover:border-red-600 hover:text-red-600">X</button>
-                    </div>
-                    <div className=" my-2 lg:my-5">
-                        <p className=" text-gray-400"> Cash on delivery <span className="text-gray-500 font-semibold">(COD)</span> is <span className="text-gray-500 font-semibold">not available</span> for any product, the payment will only be accepted in crypto coins mentioned in  check out process</p>
-                    </div>
-                </div>
-            </div>
+            <button
+              onClick={() => onClose()}
+              className=" font-bold text-sm border-2 px-2 rounded-md hover:border-red-600 hover:text-red-600"
+            >
+              X
+            </button>
+          </div>
+          <div className=" my-2 lg:my-5">
+            <p className=" text-gray-400">
+              {" "}
+              Cash on delivery{" "}
+              <span className="text-gray-500 font-semibold">(COD)</span> is{" "}
+              <span className="text-gray-500 font-semibold">not available</span>{" "}
+              for any product, the payment will only be accepted in crypto coins
+              mentioned in check out process
+            </p>
+          </div>
         </div>
-     );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default CodModal;
--- Components/SizeAndLinkForm.js
+++ Components/SizeAndLinkForm.js
@@ -1,63 +1,66 @@
+import { useState } from "react";
 import { FaLongArrowAltRight } from "react-icons/fa";
 
 const BuyForm = () => {
+  const size = ["S", "M", "L", "XL", "XXL", "XXXL"];
 
-  const submitHandler = () => {
-    
-  }
+  const [selectedSize, setSelectedSize] = useState(size[0]);
+  const [nftUrl, setNftUrl] = useState("");
+
+  var expression =
+    "https?://(?:www.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|www.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|https?://(?:www.|(?!www))[a-zA-Z0-9]+.[^s]{2,}|www.[a-zA-Z0-9]+.[^s]{2,}";
+  var regex = new RegExp(expression);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const isvalid = regex.test(nftUrl);
+    console.log("isvalid", isvalid);
+  };
 
   return (
     <>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className=" mt-5">
           <label htmlFor="size">
             <h1 className=" text-slate-600 font-bold text-xl dark:text-slate-50">
-              Size
+              Size ({selectedSize})
             </h1>
           </label>
           <div className=" py-3 -ml-1">
-            <button className="border-[3px] text-slate-500/50 font-semibold rounded-lg py-1 px-3 lg:text-lg mx-1 hover:text-red-500 hover:border-red-500 dark:text-slate-500 dark:border-slate-500 dark:hover:text-red-500 dark:hover:border-red-500">
-              S
-            </button>
-
-            <button className="border-[3px] text-slate-500/50 font-semibold rounded-lg lg:text-lg py-1 px-3 mx-1 hover:text-red-500 hover:border-red-500 dark:text-slate-500 dark:border-slate-500 dark:hover:text-red-500 dark:hover:border-red-500 ">
-              M
-            </button>
-
-            <button className="border-[3px] text-slate-500/50 font-semibold rounded-lg lg:text-lg py-1 px-3 mx-1 hover:text-red-500 hover:border-red-500 dark:text-slate-500 dark:border-slate-500 dark:hover:text-red-500  dark:hover:border-red-500">
-              L
-            </button>
-
-            <button className="border-[3px] text-slate-500/50 font-semibold rounded-lg lg:text-lg py-1 px-3 mx-1 hover:text-red-500 hover:border-red-500 dark:text-slate-500 dark:border-slate-500 dark:hover:text-red-500 dark:hover:border-red-500 ">
-              XL
-            </button>
-
-            <button className="border-[3px] text-slate-500/50 font-semibold rounded-lg lg:text-lg py-1 px-3 mx-1 hover:text-red-500 hover:border-red-500 dark:text-slate-500 dark:border-slate-500 dark:hover:text-red-500 dark:hover:border-red-500 ">
-              XXL
-            </button>
-
-            <button className="border-[3px] text-slate-500/50 font-semibold rounded-lg lg:text-lg py-1 px-3 mx-1 hover:text-red-500 hover:border-red-500 dark:text-slate-500 dark:border-slate-500 dark:hover:text-red-500 dark:hover:border-red-500 ">
-              XXXL
-            </button>
+            {size.map((sizes) => (
+              <button
+                onClick={() => setSelectedSize(sizes)}
+                className={`border-[3px] ${
+                  sizes === selectedSize
+                    ? "border-red-500 text-red-500"
+                    : "text-slate-500/50"
+                } font-semibold rounded-lg py-1 px-3 lg:text-lg mx-1 hover:text-red-500 hover:border-red-500 dark:text-slate-500 dark:border-slate-500 dark:hover:text-red-500 dark:hover:border-red-500`}
+              >
+                {sizes}
+              </button>
+            ))}
           </div>
 
           <label htmlFor="size">
             <h1 className=" text-slate-600 font-bold text-xl dark:text-slate-50">
-              NFT image Link
+              NFT Link
             </h1>
           </label>
           <input
             type="text"
             name="link"
+            value={nftUrl}
+            onChange={(e) => setNftUrl(e.target.value)}
+            required
             placeholder="Enter nft link"
             className=" focus:border-red-500 bg-transparent border-[3px] mt-2 p-2 w-full rounded-lg dark:border-slate-500 dark:focus:border-red-500"
           />
-
-          <div className=" text-slate-50 bg-red-500 hover:bg-red-600 cursor-pointer font-semibold rounded-xl w-full text-center mt-5 text-lg">
-            <button onClick={submitHandler} className=" scale-100 hover:scale-105 p-[9px]">
-              BUY NOW
-            </button>
-          </div>
+          <button
+            type="submit"
+            className=" scale-100 hover:scale-105 p-[9px] text-slate-50 bg-red-500 hover:bg-red-600 cursor-pointer font-semibold rounded-xl w-full text-center mt-5 text-lg"
+          >
+            BUY NOW
+          </button>
         </div>
       </form>
     </>
