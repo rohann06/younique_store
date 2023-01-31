daf2b9adb588bfe39d0d6073d9f92ae1b8d27640
Rohan <rohanahire651@gmail.com>
2023-02-01 02:56:51 +0530

    user id added

--- Components/CheckoutComp.js
+++ Components/CheckoutComp.js
@@ -63,6 +63,7 @@ const CheckoutComp = ({ product, size, link }) => {
     // console.log(supabase);
     const res = await supabase.from("orders").insert([
       {
+        user_id:address,
         name: userName,
         productName: name,
         productId: _id,
